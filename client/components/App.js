5316d2bf1ac8adfb99569abd11e8d4fbed27b7ee
Dwayne Neckles <dwayneaneckles@gmail.com>
2017-05-29 20:24:17 -0400

    wiring up the ui component to generate rows

--- client/components/App.js
+++ client/components/App.js
@@ -2,6 +2,27 @@ import React, { Component } from "react";
 import Row from "./Row";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      numberOfUsers: 7 ,
+       numberOfRows: 7 
+    }
+    this.handleClick  =  this.handleClick.bind(this)
+    this.renderRow    =  this.renderRow.bind(this)
+  }
+
+  handleClick () {
+    console.log("test"+this.state.numberOfRows)
+    this.setState({numberOfRows: this.state.numberOfRows+1})
+  }
+
+  renderRow () {
+    for (let i=0; i<this.state.numberOfRows; i++) {
+      return <Row number={i}/>
+    }
+  }
+
   render() {
     return (
       <div>
@@ -12,12 +33,13 @@ export default class App extends React.Component {
               <button>Save</button></td>
             </tr>
             <tr>
-              <td>Enter your JSON style here: </td>
+              <td>Enter your JSON style here: {this.state.numberOfRows} </td>
               <td>Here is what that would look like:</td>
             </tr>
-           <Row number="0"/>
+            {this.renderRow() }
+
             <tr>
-              <td colSpan={2} className="last"> <button>Add Row</button> </td>
+              <td colSpan={2} className="last"> <button onClick={this.handleClick}>Add Row</button> </td>
               
             </tr>
           </tbody>
